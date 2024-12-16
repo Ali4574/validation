d330090a91afeabddb8716a1c8cb1cf721bc539f
ALISH SHAIKH <131742793+Ali4574@users.noreply.github.com>
2024-12-16 11:35:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,6 @@ module.exports = {
   AWS_REGION: process.env.AWS_REGION,
   REDIS_HOST: process.env.REDIS_HOST,
   REDIS_PORT: parseInt(process.env.REDIS_PORT, 10),
+  MONGO_URI: process.env.MONGO_URI,
+  MONGO_DB_NAME: process.env.MONGO_DB_NAME
 };
